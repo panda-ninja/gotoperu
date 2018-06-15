821bbabbb2825e605970a3d53c782d36cfc2c9d4
freddy <fredy1432@hotmail.com>
2018-06-15 10:14:57 -0500

    cambios

--- app/Http/Controllers/ServicesController.php
+++ app/Http/Controllers/ServicesController.php
@@ -38,6 +38,9 @@ class ServicesController extends Controller
         }
         $posTipo = $request->input('posTipo');
         $txt_localizacion = $request->input('txt_localizacion_' . $posTipo);
+        if(strlen($txt_localizacion))
+            $txt_localizacion='';
+
         if ($posTipo == 0) {
             $S_2 = $request->input('S_2');
             $D_2 = $request->input('D_2');
